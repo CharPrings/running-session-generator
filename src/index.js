a2a4802a34aa1c4e96f7397e415523ad69576945
CharPrings <132065358+CharPrings@users.noreply.github.com>
2024-09-11 18:42:03 +0100

    Added the AI API integration and form edits

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generateSession(event) {
-    event.preventDefault();
-
+function displaySession(response) {
     new Typewriter('#session', {
-        strings: ['Answer here'],
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
       });
+}
+
+function generateSession(event) {
+    event.preventDefault();
+
+    let instructionInput = document.querySelector("#user-instructions")
+    let apiKey = "ba30aa45f9d6647cd786f84aoc0t3a2d";
+    let prompt = `User: Generate a ${instructionInput.value} minutes speedwork session`
+    let context = "You are a running coach with a speciality in speedwork. Your mission is to generate a summarised speedwork session, including a warm up and cool down. Show the response in basic HTML but do not include the word HTML. The user will provide the length of the session in minutes."
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+    axios.get(apiUrl).then(displaySession);
+
+    
 }
 
 let sessionFormElement = document.querySelector("#session-generator");
